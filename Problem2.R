2847491151897633438d1ac53df86f357d9eb522
zhenhanzhao <zzhao13@jhu.edu>
2012-12-10 14:09:54 -0400

    COMPILED

--- Problem2.R
+++ Problem2.R
@@ -1,6 +1,7 @@
+setwd("C:/Users/George/zzhao1312102012.git")
 Problem2<-read.table('rawdata.csv',sep=',',header=T)
 attach(Problem2)
 Problem2<-tapply(Medal,INDEX=NOC,FUN=length)
 y<-tapply(Medal,INDEX=NOC,FUN=table)
 
-save(file='Problem2.RData',list='problem2')
+save(file='Problem2.RData',list='Problem2')
